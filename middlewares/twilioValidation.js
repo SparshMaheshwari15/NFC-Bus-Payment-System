5ac1751ef882589b6f0b00387af49aba133e78ff
Sparsh Maheshwari <sparshmaheshwari15@gmail.com>
2024-10-24 01:25:33 +0530

    validate twilio req

---
+++ middlewares/twilioValidation.js
@@ -0,0 +1,20 @@
+const twilio = require('twilio');
+
+const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN; // Replace with your actual Auth Token
+
+// Middleware to validate requests from Twilio
+function validateTwilioRequest(req, res, next) {
+    const twilioSignature = req.headers['x-twilio-signature'];
+    const url = req.protocol + '://' + req.get('host') + req.originalUrl;
+    const requestBody = req.body;
+
+    const isValid = twilio.validateRequest(TWILIO_AUTH_TOKEN, twilioSignature, url, requestBody);
+
+    if (!isValid) {
+        return res.status(403).send('Forbidden: Invalid request signature.');
+    }
+
+    next();
+}
+
+module.exports = validateTwilioRequest;
--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { sendMsg } = require("../controllers/whatsapp");
+const validateTwilioRequest = require("../middlewares/twilioValidation");
 const router = express.Router();
 
 // Route to render the EJS page with users
-router.post("/", sendMsg);
+router.post("/", validateTwilioRequest,sendMsg);
 
 module.exports = router;
