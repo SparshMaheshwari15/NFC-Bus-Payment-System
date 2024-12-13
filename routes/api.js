05ac2ce283b6b925beb85e49e822686a37d0f0cd
Sparsh Maheshwari <sparshmaheshwari15@gmail.com>
2024-12-13 23:32:42 +0530

    Routes formatted

--- routes/api.js
+++ routes/api.js
@@ -1,21 +1,6 @@
 const express = require("express");
-const {
-    getAllUsers,
-    addUser,
-    toggleUserStatus,
-    deleteUser,
-    registerUser,
-    driverLogin,
-    userLogin,
-    logout,
-    updateUser,
-} = require("../controllers/user.js");
-
-const {
-    deductBalanceAdmin,
-    deductBalanceBus,
-    addBalance,
-} = require("../controllers/balance.js");
+const userController = require("../controllers/user.js");
+const balanceController = require("../controllers/balance.js");
 const { validateUser } = require("../middleware.js");
 const authenticateJWT = require("../utils/auth.js");
 const passport = require("passport");
@@ -29,25 +14,33 @@ const router = express.Router();
 
 // Route to get all users as JSON
 // Keep this for API
-router.get("/users", getAllUsers);
+router.get("/users", userController.getAllUsers);
 
 // Route to deduct balance
 router.post(
     "/users/deductBalanceBus",
     // isDriver,
     authenticateJWT,
-    deductBalanceBus
+    balanceController.deductBalanceBus
 );
 
 // Route to add balance
-router.post("/users/addBalance", isAdmin, addBalance);
-router.post("/users/addUser", isAdmin, validateUser, addUser);
-router.post("/users/deductBalanceAdmin", isAdmin, deductBalanceAdmin);
-router.post("/users/manage/toggle", isAdmin, toggleUserStatus);
-router.delete("/users", isAdmin, deleteUser);
-router.patch("/users", isAdmin, updateUser);
-router.post("/users/login/driver", authenticateDriver, driverLogin);
-router.post("/users/signup", isAdmin, registerUser);
+router.post("/users/addBalance", isAdmin, balanceController.addBalance);
+router.post("/users/addUser", isAdmin, validateUser, userController.addUser);
+router.post(
+    "/users/deductBalanceAdmin",
+    isAdmin,
+    balanceController.deductBalanceAdmin
+);
+router.post("/users/manage/toggle", isAdmin, userController.toggleUserStatus);
+router.delete("/users", isAdmin, userController.deleteUser);
+router.patch("/users", isAdmin, userController.updateUser);
+router.post(
+    "/users/login/driver",
+    authenticateDriver,
+    userController.driverLogin
+);
+router.post("/users/signup", isAdmin, userController.registerUser);
 
 router.post(
     "/users/login",
@@ -56,8 +49,8 @@ router.post(
         failureRedirect: "/users/login",
         failureFlash: true,
     }),
-    userLogin
+    userController.userLogin
 );
 
-router.get("/users/logout", logout);
+router.get("/users/logout", userController.logout);
 module.exports = router;
